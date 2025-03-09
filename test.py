d02fbcead4ec03f5b8ec682ca7011747618c3255
Elias Liassides <elias2.liassides@gmail.com>
2025-03-09 14:11:43 +0000

    better resolution of images

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from PIL import Image
 import os
 
-input_folder = "./assets/"
-output_folder = "./assets/webp/"
+input_folder = "./"
+output_folder = "./"
 os.makedirs(output_folder, exist_ok=True)
 
 for filename in os.listdir(input_folder):
-    if filename.endswith(".jpg") or filename.endswith(".png"):
+    if filename.startswith("image"):
         img = Image.open(input_folder + filename)
         webp_filename = os.path.splitext(filename)[0] + ".webp"
-        img.save(output_folder + webp_filename, "WEBP", quality=80)
+        img.save(output_folder + webp_filename, "WEBP", quality=100)
 print("Conversion Complete!")
